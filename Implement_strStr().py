7d4c67899c62e04fbdfbc341a66e387b9c2f054e
shamiOuO <93506900+shamiOuO@users.noreply.github.com>
2021-11-06 00:44:29 +0800

    Add files via upload

--- Implement_strStr().py
+++ Implement_strStr().py
@@ -1,11 +1,10 @@
 def strStr(self, haystack: str, needle: str) -> int:
     if needle == "":
-        print(0)
-    count =0
+        return 0
     if needle in haystack:
-        print(needle)
+        return haystack.index(needle)
     else:
-        print(-1)
+        return -1
 
 
-strStr(self = '',haystack = 'aaaaa',needle = 'bba')
+print(strStr(self = '',haystack = 'hello',needle = 'll'))
